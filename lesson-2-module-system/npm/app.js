d47e1e74d3b393fb78d7bb0933ce1478da0ede84
AnwarHossainSR <mahedianwar@gmail.com>
2021-11-25 18:46:16 +0600

    user input

--- lesson-2-module-system/npm/app.js
+++ lesson-2-module-system/npm/app.js
@@ -1,5 +1,15 @@
 const validator = require("validator");
 const chalk = require("chalk");
 
-console.log(validator.isEmail("mahedianwar@Gmail.com"));
-console.log(chalk.blue("error"));
+// console.log(validator.isEmail("mahedianwar@Gmail.com"));
+// console.log(chalk.blue("error"));
+
+const command = process.argv[2];
+
+console.log(process.argv);
+
+if (command === "add") {
+  console.log("Adding note!");
+} else if (command === "remove") {
+  console.log("Removing note!");
+}
